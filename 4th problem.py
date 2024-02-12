9b857dd3186bfb75cd8d28dedb9120dbb05ba999
Ayan <142248259+Ayan56789@users.noreply.github.com>
2024-02-12 21:11:51 +0530

    4th problem.py
    for bisect method

---
+++ 4th problem.py
@@ -0,0 +1,93 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+from scipy.optimize import newton, bisect, brentq
+from scipy.constants import hbar, m_e
+# Defining helper functions
+
+# (1) Potential function
+
+def V(r, r0, α, D):
+    V = D*(1 - np.exp(-α*(r - r0)))**2
+    return V
+
+# (2) Defining A(r)
+
+def A(r):
+    a = (2*μ/ħc**2)*(V(r, r0, α, D) - E)
+    return a
+
+# (3) Defining Derivative functions of 'y'
+
+def deriv(y, r):
+    return np.array([y[1], A(r)*y[0]])
+
+# (4) Defining wave function with respect to energy
+
+def waveFunc(energy):
+    global E
+    global psi
+    E = energy
+    psi = odeint(deriv, psi0, r)
+    return psi[-1, 0]
+
+# (5) Finding zero of the wavefuncton by Shooting method
+
+def zero(x, y):
+    energyEigenValue = []
+    s = np.sign(y)
+    #s = np.array(s, dtype=int)
+    for i in range(len(s)-1):
+        if s[i] == -s[i+1]:
+            zero = bisect(waveFunc, x[i], x[i+1])
+            energyEigenValue.append(zero)
+    return energyEigenValue
+
+# Vectorizing the functions
+V        = np.vectorize(V)
+waveFunc = np.vectorize(waveFunc)
+zero = np.vectorize(zero)
+# Important parameters
+
+μ = 940e6
+D = 0.755501
+α = 1.44
+r0 = 0.131349
+ħc = 1973
+
+# Initialisation
+
+N = 100                              # Number of points on x-axis
+psi = np.zeros([N, 2])                # To store wave function values and its derivative
+psi0 = np.array([0.001, 2])           # wavefunction of initial staes
+
+
+r = np.linspace(-0.2, 0.2, 100)              # x - axis
+En = np.arange(0, 15, 1)
+psiRight = waveFunc(En)
+#psiRight
+def zero(x, psi):
+    energyEigenValue = []
+    s = np.sign(psi)
+    #s = np.array(s, dtype=int)
+    for i in range(len(s)-1):
+        if s[i] == -s[i+1]:
+            zero = bisect(waveFunc, x[i], x[i+1])
+            energyEigenValue.append(zero)
+    return energyEigenValue
+EigenValue = np.array(zero(En, psiRight))
+EigenValue
+array([ 1.21457505,  2.10732938,  3.2543345 ,  4.65597459,  6.31235437,
+        8.22351125, 10.389463  , 12.81021612])
+fig = plt.figure(figsize=(5, 15))
+fig.subplots_adjust(hspace=0.4, wspace=0.4)
+j=1
+for i in range(len(EigenValue)):
+    ax = fig.add_subplot(len(EigenValue), 1, j)
+    waveFunc(EigenValue[i])
+    plt.plot(r, psi.transpose()[0])
+    #plt.legend()
+    plt.xlabel('$r$', fontsize=14)
+    plt.ylabel('$\psi(r)$', fontsize=14)
+    j+=1
+plt.show()
